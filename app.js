f14dc4f4df6d56a0781db15c50f389461e6effd5
nitish01-test <nitish@i2vsys.com>
2025-05-04 20:16:00 +0530

    middleware

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use((req, res, next) => {
+  console.log("This is middleware");
+  next();
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
